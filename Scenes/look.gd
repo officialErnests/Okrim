38619c29039815c11e60b698d652c5cb19177cfe
n0o0b090lv <ernestspursw@gmail.com>
2025-07-31 22:10:16 +0300

    Movement

--- Scenes/look.gd
+++ Scenes/look.gd
@@ -3,14 +3,17 @@ extends Camera3D
 @onready var light = $"../PlayerUtil/PointLight"
 @onready var tp_indicator = $"../PlayerUtil/Tp"
 @onready var spot_light =$SpotLight3D
+var selected_material = preload("res://Scenes/Shaders/selected.tres")
 var speed = 500
 const RAY_LENGTH = 1000
 var mouseDebounce = true
 var animation_switch = "End"
+var last_selection : Area3D
+var last_selected = false
 func _process(delta: float) -> void:
 	var normilized_mouse = Vector2(0.5,0.5) - (get_viewport().get_mouse_position() / Vector2(get_viewport().get_visible_rect().size))
 	if normilized_mouse.x > 0:
-		normilized_mouse.x = normilized_mouse.x - 0.1
+		normilized_mouse.x = normilized_mouse.x - 0.1 
 		if normilized_mouse.x < 0: normilized_mouse.x = 0
 	else:
 		normilized_mouse.x = normilized_mouse.x + 0.1
@@ -31,6 +34,7 @@ func _process(delta: float) -> void:
 	var result = space_state.intersect_ray(query)
 
 	var ray_hit = false
+	last_selected = false
 	if animation_switch == "End" and not tp_indicator.get_node("AnimatedSprite3D").is_playing():
 		animation_switch = "Start_End"
 		position = tp_indicator.position + Vector3(0,2,0)
@@ -39,8 +43,8 @@ func _process(delta: float) -> void:
 		# light.global_position.y = 2
 		spot_light.look_at(result.position)
 
-		if result.collider.is_in_group("Move_zone"):
-			if not(animation_switch == "End" and tp_indicator.get_node("AnimatedSprite3D").is_playing()):
+		if not(animation_switch == "End" and tp_indicator.get_node("AnimatedSprite3D").is_playing()):
+			if result.collider.is_in_group("Move_zone"):
 				ray_hit = true
 				tp_indicator.position = result.position
 				tp_indicator.visible = true
@@ -51,6 +55,19 @@ func _process(delta: float) -> void:
 					else:
 						animation_switch = "Start"
 						tp_indicator.get_node("AnimatedSprite3D").play("Start")
+			if result.collider.is_in_group("Interactable"):
+				last_selected = true
+				if result.collider != last_selection:
+					if last_selection:
+						for selected_mesh_iter in last_selection.get_parent().get_child(0).get_children():
+							selected_mesh_iter.material_overlay = null
+					last_selection = result.collider
+					for selected_mesh_iter in last_selection.get_parent().get_child(0).get_children():
+						selected_mesh_iter.material_overlay = selected_material
+	if not last_selected and last_selection:
+		for selected_mesh_iter in last_selection.get_parent().get_child(0).get_children():
+			selected_mesh_iter.material_overlay = null
+		last_selection = null
 	if not ray_hit:
 		if not (animation_switch == "Start_end" or animation_switch == "End"):
 			animation_switch  = "Start_end"
